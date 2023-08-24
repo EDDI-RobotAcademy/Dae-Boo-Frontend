00b7a15344bbe8f382688d082ec928204e5fe08d
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-08-24 12:43:20 +0900

    update: [장현민] vue3초기 세팅 변경 [PMF-1]

---
+++ src/router/error/index.js
@@ -0,0 +1,9 @@
+import ErrorPage from '@/views/error/ErrorPage.vue'
+const errorRouter = [
+    {
+        path: '/error-page',
+        name: 'ErrorPage',
+        component: ErrorPage
+      },
+]
+export default errorRouter
---
+++ src/router/main/index.js
@@ -0,0 +1,9 @@
+import MainPage from '@/views/main/MainPage.vue'
+const mainRouter = [
+    {
+        path: '/',
+        name: 'MainPage',
+        component: MainPage
+      },
+]
+export default mainRouter
