5398b4543f87bafbfee00981a83d5c8daa43eda2
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-08-24 12:46:04 +0900

    update: [장현민] vue3초기 세팅 변경 추가 [PMF-1]

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <v-app>
+  <div class="back-ground-color">
+  <v-app class="main-container">
     <v-main>
+      <navigation-bar/>
       <router-view/>
     </v-main>
+    <navigation-foot-bar/>
   </v-app>
+</div>
 </template>
 
 <script>
-
+import NavigationBar from '@/views/navigation/NavigationBar.vue'
+import NavigationFootBar from '@/views/navigation/NavigationFootBar.vue'
 export default {
   name: 'App',
+  components: {
+    NavigationBar,
+    NavigationFootBar
+  },
 
   data: () => ({
     //
   }),
 }
 </script>
+
+<style>
+.back-ground-color{
+  background-color: #222224;
+}
+.main-container{
+  max-width: 1500px;
+  background-color: #222224;
+  margin: auto;  
+}
+</style>
