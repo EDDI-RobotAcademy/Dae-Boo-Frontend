accf4ce86bee4c2e9c762c7ff954339926cf87c0
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-08-28 15:00:02 +0900

    유의사항 페이지 생성 [PMF-18]

---
+++ src/components/help/CautionForm.vue
@@ -0,0 +1,88 @@
+<template>
+    <v-container class="caution-mt" justify="center">
+    <v-card rounded class="caution-color">
+        <h2 class="caution-title"> 유의사항 </h2>
+        <div class="caution-text">
+        <p class="text-mt">* 해외에서 카드 결제 시, 현지 통화가 아닌 원화로 결제하는 경우 해외원화결제서비스(DCC) 수수료가 추가로 발생할 수 있으므로 유의하시기 바랍니다.</p>
+        <p class="text-mt"> - 해외원화결제서비스(DCC) 차단방법 : 홈페이지(PC) 또는 앱 → 전체메뉴 → 고객센터 → 해외 이용 → 해외 이용 잠금 서비스 </p>
+        <p class="text-mt">* 해외 이용 시(해외 사이트 거래 포함) 미화(USD) 기준 거래미화금액에 접수일의 우리은행 최초 고시 전신환매도율을 적용한 후, <br>국제브랜드사가 부과하는 브랜드수수료(Mastercard 1%)와 삼성카드가 부과하는 해외이용수수료(0.2%)를 합산하여 원화로 청구됩니다.</p>
+        <p class="text-mt">- 브랜드사수수료의 경우 국제브랜드사 정책에 따라 변경될 수 있습니다.</p>
+        <p class="text-mt">* 해외 이용 시 청구금액 산출방법은 아래와 같습니다.</p>
+        <p class="text-mt">- 해외 이용 시 청구금액 = (거래미화금액 × 전신환매도율①) + 브랜드사수수료② + 해외이용수수료③</p>
+        <p class="text-mt">① 전신환매도율 : 접수일의 우리은행 최초 고시 전신환매도율</p>
+        <p class="text-mt">② 브랜드사수수료 = (거래미화금액 × 국제브랜드 이용수수료율 1%) × 전신환매도율</p>
+        <p class="text-mt">③ 해외이용수수료 = (거래미화금액 × 해외서비스 수수료율 0.2%) × 전신환매도율</p>
+        <p class="text-mt">상환 능력에 비해 신용카드 사용액이 과도할 경우 귀하의 개인신용평점이 하락할 수 있습니다.</p>
+        <p class="text-mt">개인신용평점 하락 시 금융거래 관련된 불이익이 발생할 수 있습니다.</p>
+        <p class="text-mt">일정 기간 원리금을 연체할 경우, 모든 원리금을 변제할 의무가 발생할 수 있습니다.</p>
+        <p class="text-mt">금융상품 이용 전 상품설명서, 홈페이지, 약관을 통해 이용조건을 확인해 주시기 바랍니다.</p>
+        <p class="text-mt">금융소비자는 해당 상품 또는 서비스에 대하여 설명을 받을 권리가 있습니다.</p>
+        <p class="text-mt">신용카드 발급이 부적정한 경우(개인신용평점 낮음 등) 카드 발급이 제한될 수 있습니다.</p>
+        <p class="text-mt">카드 이용대금과 이에 수반되는 모든 수수료를 지정된 대금 결제일에 상환합니다.</p>
+        <p class="text-mt">필요 이상으로 신용카드를 발급 및 이용하실 경우 개인신용평점, 이용한도 등에 영향을 미칠 수 있습니다.</p>
+    </div>
+</v-card>
+<div class="caution-mt">
+<v-card rounded="" class="caution-color">
+    <div class="caution-text2">
+        <p class="text-mt">계약을 체결하기 전에 자세한 내용은 해당 카드사의 상품설명서와 약관을 읽어보시기 바랍니다.</p>
+        <p class="text-mt">“금융소비자는 해당상품에 대하여 설명을 받을 권리가 있으며, 그 설명을 듣고 내용을 충분히 이해한 후 거래하시기 바랍니다.”</p>
+        <p class="text-mt">“신용카드 발급이 부적정한 경우, 금융회사의 심사기준(개인신용평점 기준 등)에 따라 카드발급이 제한될 수 있습니다.”</p>
+        <p class="text-mt">“카드이용대금과 이에 수반되는 모든 수수료를 지정된 대금 결제일에 상환합니다.”</p>
+        <p class="text-mt">“카드 이용대금 연체 시 연체이자가 부과될 수 있으며, 상세한 부과기준은 상품별 상세내용을 참조하여 주시기 바랍니다”</p>
+        <p class="text-mt">“상환능력에 비해 신용카드 사용액이 과도할 경우, 귀하의 개인신용평점이 하락할 수 있습니다.”</p>
+        <p class="text-mt">“개인신용평점 하락 시 금융거래와 관련된 불이익이 발생할 수 있습니다.”</p>
+        <p class="text-mt">“일정기간 원리금을 연체할 경우, 모든 원리금을 변제할 의무가 발생할 수 있습니다.”</p>
+        <p class="text-mt">위 정보는 카드사가 제공한 정보로 각 카드사의 사정에 따라 제한, 변경, 폐지 될 수 있습니다.</p>
+        <p class="text-mt">본 서비스는 광고 서비스로서, 신용카드 신청 및 발급 등 금융상품판매는 카드사 홈페이지에서 이루어집니다.</p>
+        <p class="text-mt">신용카드 신청 및 발급 등 금융상품판매에 관한 책임은 개별 카드사에 있으며, MOCA는 이에 관한 어떠한 책임도 부담하지 않습니다.</p>
+        <p class="text-mt">[신용카드상품 판매 대리·중개에 해당하는 경우에 한하여 아래 내용을 추가로 고지합니다]</p>
+        <p class="text-mt">"MOCA는 아래의 금융회사를 대리하거나 중개하며, 동 금융회사의 신용카드 회원모집 업무를 대리·중개하고 있습니다."</p>
+        <p class="text-mt">· ㈜KB국민카드, 하나카드㈜, 삼성카드㈜</p>
+        <p class="text-mt">"MOCA는 위 금융회사의 금융상품에 대한 계약체결권을 부여받지 않았습니다. (신용카드 상품 계약은 해당 금융회사와 직접 체결됩니다)"</p>
+        <p class="text-mt">"MOCA는 금융관계법률에 따라 신용카드사와 위탁계약을 체결한 금융상품판매대리·중개업자입니다."</p> 
+    </div>
+</v-card>
+</div>
+</v-container>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="css" scoped>
+.caution-mt{
+    margin-top: 3rem;
+    margin-bottom: 3rem;
+}
+.caution-color{
+    margin: auto;
+    color: white;
+    background-color: #444444;
+    width: 80%;
+    border-radius: 1rem;
+}
+.caution-title{
+    margin-top: 2rem;
+    margin-left: 1rem;
+    margin-bottom: 1rem;
+    
+}
+.caution-text{
+    margin-bottom: 2rem;
+    border-top: 1px solid  #ffffff;  
+    padding: 1rem;
+    color: #d2d2d2;
+}
+.caution-text2{
+    margin-bottom: 2rem;
+    padding: 1rem;
+    color: #d2d2d2;
+}
+.text-mt{
+    margin-top: 1rem;
+}
+</style>
---
+++ src/views/help/CautionPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <caution-form/>
+    </div>
+</template>
+
+<script>
+import CautionForm from '@/components/help/CautionForm.vue'
+    export default {
+  components: { CautionForm },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
