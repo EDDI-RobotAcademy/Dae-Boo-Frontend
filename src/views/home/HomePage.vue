df8ef24bc60ad924a9438c32256c68f6d07ea119
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-08-28 14:58:39 +0900

    feat: [장현민] HomePage 초기 세팅 [PMF-17]

---
+++ src/views/home/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <home-form/>
+    </div>
+</template>
+
+<script>
+import HomeForm from '@/components/home/HomeForm.vue'
+export default {
+    components: {
+        HomeForm
+    },
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
