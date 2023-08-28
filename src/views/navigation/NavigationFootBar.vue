369c6b5e6b3c8bde2e01ffbae51cfb907ce504ef
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-08-28 14:50:19 +0900

    update: [장현민]footer-navigation css추가 [PMF-5]

--- src/views/navigation/NavigationFootBar.vue
+++ src/views/navigation/NavigationFootBar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer class="footer-content">
     <v-row  no-gutters>
-        <v-btn class="footer-name">
+        <v-btn class="footer-name" @click="goFAQPage">
         <h2 class="text-h5">FAQ</h2>
       </v-btn>
         <v-btn class="footer-name2">
@@ -16,7 +16,7 @@
         <h3 class="head-mr">NOTICE</h3>
         <div style="font-size: 14px;">
         <router-link to="/error-page" class="custom-link">
-          <p>[공지] 신청/취소 관련 사용 정책 변경 사전 안내</p>
+        <p>[공지] 신청/취소 관련 사용 정책 변경 사전 안내</p>
         </router-link>
         <router-link to="/error-page" class="custom-link">
         <p>[공지] 안전한 MOCA 서비스 이용을 위한 비밀 번호 변경 방법 안내</p>
@@ -44,7 +44,7 @@
 
       </v-col>
       <v-col class="footer-item2" cols="2">
-        <h3 class="head-mr">MY ORDER</h3>
+        <h3 class="head-mr">MY MOCA</h3>
         <router-link to="/error-page" class="custom-link">
           <p>신청/취소 내역</p>
         </router-link>
@@ -72,20 +72,19 @@
           <p>마케팅 제휴, 입점 문의</p>
         </router-link>
         <router-link to="/error-page" class="custom-link">
-          <p>카드 Q&A 내역</p>
-        </router-link>
-        <router-link to="/error-page" class="custom-link">
           <p>공지사항</p>
         </router-link>
         <router-link to="/error-page" class="custom-link">
+          <p>카드 Q&A 내역</p>
+        </router-link>
+        <router-link to="/help/faq" class="custom-link">
           <p>FAQ</p>
         </router-link>
-        <router-link to="/error-page" class="custom-link">
-          <p>고객의 소리</p>
+        <router-link to="/help/caution" class="custom-link">
+          <p>유의사항</p>
           <br>
         </router-link>
       </v-col>      
-
 
       <h3 class="footer-item3-main">개인정보처리방침 | 이용약관</h3>
       <v-col cols="12">
@@ -97,10 +96,9 @@
           개인정보 보호책임자 : 김모카 |
           호스팅 서비스 : ㈜ MOCA
         </p>
-        <p class="footer-item3 item3-mt">일부 상품의 경우 MOCA 는 통신 판매의 당사자가 아닌 통신판매중개자로서 상품, 상품정보, 거래에 대한 책임이 제한될 수 있으므로, 각 상품 페이지에서 구체적인  내용을 확인하
-          <br>
-          시기 바랍니다.
+        <p class="footer-item3 item3-mt">일부 상품의 경우 MOCA 는 통신 판매의 당사자가 아닌 통신판매중개자로서 상품, 상품정보, 거래에 대한 책임이 제한될 수 있으므로, 각 상품 페이지에서 구체적인  내용을 확인하시기 바랍니다.
         </p>
+        <br>
       </v-col>
     </v-row>
   </v-footer>
@@ -111,6 +109,12 @@ import '@/assets/css/navigation/Footer.css'
 export default {
   setup() {
 
+  },
+  methods: {
+    goFAQPage() {
+      this.$router.push('/help/faq')
+      window.scrollTo(0, 0);
+    }
   },
 };
 </script>
