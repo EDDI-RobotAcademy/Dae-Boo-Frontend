aacd8a5d969bf1590deaa7f09c935d82e409b981
cjw0824 <128335544+cjw0824@users.noreply.github.com>
2023-08-29 15:27:33 +0900

    feat:[최지원] 내 정보관리 페이지 구성 [PMF-20]

--- src/components/myPage/MyPageInfo.vue
+++ src/components/myPage/MyPageInfo.vue
@@ -1,10 +1,58 @@
 <template>
-    <div>
-        내 정보 관리입니다.
+    <div class="container">
+        <b>내 정보 관리</b>
+        <!-- <v-btn class="editBtn" append-icon="mdi-pencil-outline" variant="text"></v-btn> -->
+        <span class="editBtn mdi mdi-pencil-outline" variant="text"></span>
+        <v-table class="table">
+            <tbody>
+                <tr>
+                    <th scope="row">성명</th>
+                    <td>{{ userInfo.name }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">아이디</th>
+                    <td>{{ userInfo.id }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">비밀번호</th>
+                    <td>{{ userInfo.pw }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">휴대폰 번호</th>
+                    <td>{{ userInfo.tel }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">이메일</th>
+                    <td>{{ userInfo.email }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">주소</th>
+                    <td>{{ userInfo.adress }}</td>
+                </tr>
+            </tbody>
+        </v-table>
+        <v-btn class="withdraw" append-icon="mdi-chevron-double-right" variant="text">
+            탈퇴하기
+        </v-btn>
     </div>
 </template>
 <script>
+import '@/assets/css/myPage/MyPageInfo.css'
 export default {
+    data() {
+        return {
+            userInfo:
+            {
+                name: "홍길동",
+                id: "abc123",
+                pw: "123",
+                tel: "010-0000-0000",
+                email: "abc@abc.com",
+                adress: "서울시 강남구"
+            }
+
+        }
+    },
 
 }
 </script>
--- src/views/mypage/MyPage.vue
+++ src/views/mypage/MyPage.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="container">
         <div class="box" id="box1">
-            <div class="line">
+            <div class="line box3" @click="pageNumber = 1">
                 <p class="myPageNavi">내 정보 관리</p>
-                <button class="edgeButton mdi mdi-chevron-right" @click="pageNumber = 1"></button>
+                <button class="edgeButton mdi mdi-chevron-right"></button>
             </div>
-            <div class="line">
+            <div class="line box3" @click="pageNumber = 2">
                 <p class="myPageNavi">게시글 관리</p>
-                <button class="edgeButton mdi mdi-chevron-right" @click="pageNumber = 2"></button>
+                <button class="edgeButton mdi mdi-chevron-right"></button>
             </div>
-            <div class="line">
+            <div class="line box3" @click="pageNumber = 3">
                 <p class="myPageNavi">내가 찜한 카드</p>
-                <button class="edgeButton mdi mdi-chevron-right" @click="pageNumber = 3"></button>
+                <button class="edgeButton mdi mdi-chevron-right"></button>
             </div>
-            <div class="line" style="padding-bottom: 15px;">
+            <div class="line box3" style="padding-bottom: 15px;" @click="pageNumber = 4">
                 <p class="myPageNavi">1:1 문의</p>
-                <button class="edgeButton mdi mdi-chevron-right" @click="pageNumber = 4"></button>
+                <button class="edgeButton mdi mdi-chevron-right"></button>
             </div>
         </div>
         <div class="box" id="box2">
