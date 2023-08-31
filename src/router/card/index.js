e4ac7c5137c323cc2d04c951fe3184ab5f9ad4d8
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-08-31 17:45:43 +0900

    feat: [이상원] 카드페이지 추가 [PMF-23]

---
+++ src/router/card/index.js
@@ -0,0 +1,9 @@
+import CardPage from "@/views/card/CardPage.vue";
+const cardRouter = [
+  {
+    path: "/card",
+    name: "CardPage",
+    component: CardPage,
+  },
+];
+export default cardRouter;
---
+++ src/views/card/CardPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <card-form />
+    </div>
+</template>
+
+<script>
+import CardForm from '@/components/card/CardForm.vue'
+export default {
+    components: {
+        CardForm
+    }
+}
+</script>
+<style>
+.card {
+    color: #ffffff;
+}
+</style>
