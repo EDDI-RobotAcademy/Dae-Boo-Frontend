acb2802c08b5dcc984956cdacd6bf74311056c85
zisoo <zisoo_3@naver.com>
2023-09-03 20:48:47 +0900

    update: [최지수] 관리자의 단일, 다중 게시물 삭제 [PMF-9]

--- src/store/board/actions.js
+++ src/store/board/actions.js
@@ -4,7 +4,7 @@ import {
 import axiosInst from '@/utility/axiosInst'
 
 export default {
-    // 게시물 받아오기
+    // 관리자 - 게시물 받아오기
     requestmManagementBoardToSpring({ commit }) {
         return axiosInst.get("/board/list")
             .then((res) => {
@@ -12,4 +12,16 @@ export default {
             return res.data;
         })
     },
+    // 관리자 - 단일, 다중 게시물 삭제하기
+    requestManagementBoardDeleteToSpring(_, boardId) {
+        console.log("payload에 담긴 데이터:", boardId);
+        const boardIds = boardId
+        console.log(boardIds)
+
+        return axiosInst.delete("/board/delete", { params: { boardId: boardIds } })
+        .then(() => {
+            alert("게시물 삭제가 완료되었습니다.")
+        })
+    },  
+
 }
