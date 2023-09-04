3a3f33899bce10aad7dca9c811e394d7e5eaf810
cjw0824 <128335544+cjw0824@users.noreply.github.com>
2023-09-04 12:17:39 +0900

    feat:[최지원] 내 정보 관리 페이지에 사용자 정보를 띄웁니다. [PMF-20]

--- src/store/myPage/actions.js
+++ src/store/myPage/actions.js
@@ -1,16 +1,7 @@
 import axiosInst from '@/utility/axiosInst'
-import {REQUEST_MY_BOARD_LIST_TO_SPRING} from '../myPage/mutation-types'
+import {REQUEST_MY_BOARD_LIST_TO_SPRING, REQUEST_MY_INFO_TO_SPRING} from '../myPage/mutation-types'
 
 export default {
-    // back에서 게시물 정보 가져오기
-    // getMyBoardtoSpring(_, payload){
-    //     console.log("userId: " + payload)
-
-    //     return axiosInst.get('/board/myBoard' , { params: { userId: payload }})
-    //     .then((res) => {
-    //         console.log("res: " + res.data[0].boardName)
-    //     })
-    // }
     getMyBoardtoSpring({ commit }, payload){
         console.log("userId: " + payload)
 
@@ -18,6 +9,14 @@ export default {
         .then((res) => {
             console.log("res: " + res.data[0].boardName)
             commit(REQUEST_MY_BOARD_LIST_TO_SPRING, res.data)
+        })
+    },
+    getMyInfoToSpring( { commit }, payload) {
+        console.log("userId: " + payload)
+        return  axiosInst.get('/user/userInfo', { params: { userId:payload }})
+        .then((res) => {
+            console.log("MyInfo res: " + res.data.name)
+            commit(REQUEST_MY_INFO_TO_SPRING, res.data)
         })
     }
 }
--- src/store/myPage/mutations.js
+++ src/store/myPage/mutations.js
@@ -1,6 +1,7 @@
 import {
     REQUEST_MY_BOARD_LIST_TO_SPRING,
     REQUEST_MY_BOARD_TO_SPRING,
+    REQUEST_MY_INFO_TO_SPRING
 } from '../myPage/mutation-types'
 
 export default {
@@ -9,5 +10,9 @@ export default {
     },
     [REQUEST_MY_BOARD_TO_SPRING] (state, receivedData) {
         state.myBoard = receivedData
+    },
+
+    [REQUEST_MY_INFO_TO_SPRING] (state, receivedData) {
+        state.myInfo = receivedData
     },
 }
