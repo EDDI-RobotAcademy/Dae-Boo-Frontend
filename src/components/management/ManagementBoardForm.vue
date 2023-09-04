d2000ea3aaca57c60be951da15445058ad2e73b2
zisoo <zisoo_3@naver.com>
2023-09-04 17:26:28 +0900

    Revert "feat: [최지수] 관리자는 게시 글 검색을 할 수 있습니다 [PMB-24]"
    This reverts commit bf1c74dc2325c85060fa063b05aca58fbe8f16a5.

--- src/components/management/ManagementBoardForm.vue
+++ src/components/management/ManagementBoardForm.vue
@@ -1,43 +1,44 @@
 <template>
     <div>
       <v-table style="margin: auto">
-      <tr>
-        <th align="center" width="10%">check</th>
-        <th align="center" width="10%">게시물 번호</th>
-        <th align="center" width="40%">제목</th>
-        <th align="center" width="20%">아이디</th>
-        <th align="center" width="20%">작성일</th>
-      </tr>
-      <tr>
-        <td colspan="5">
-          <hr style="margin: 10px auto;"> <!-- 가로 선 밑에 10px 여백 추가 -->
-        </td>
-      </tr>
-      <tr v-if="!filteredBoards || (Array.isArray(filteredBoards) && filteredBoards.length === 0)">
-        <td colspan="4">검색 결과가 없습니다.</td>
-      </tr>
-      <tr v-else v-for="board in filteredBoards" :key="board.boardId">
-        <td align="center">
-          <input type="checkbox" v-model="selectedItems" :value="board.boardId" />
-        </td>
-        <td align="center">
-          {{ board.boardId }}
-        </td>
-        <td align="center">
-          {{ board.boardName }}
-        </td>
-        <td align="center" style="padding: 10px 0;">
-          {{ board.userId.nickname }}
-        </td>
-        <td align="center" style="padding: 10px 0;">
-          {{ board.boardRegisterDate }}
-        </td>
-      </tr>
-    </v-table>
+        <tr>
+          <th align="center" width="10%">check</th>
+          <th align="center" width="10%">게시물 번호</th>
+          <th align="center" width="40%">제목</th>
+          <th align="center" width="20%">아이디</th>
+          <th align="center" width="20%">작성일</th>
+        </tr>
+        <tr>
+          <td colspan="5">
+            <hr style="margin: 10px auto;"> <!-- 가로 선 밑에 10px 여백 추가 -->
+          </td>
+        </tr>
+        <tr v-if="!boards || (Array.isArray(boards) && boards.length === 0)">
+          <td colspan="4">현재 등록된 게시물이 없습니다!</td>
+        </tr>
+        <tr v-else v-for="board in boards" :key="board.boardId">
+          <td align="center">
+            <input type="checkbox" v-model="selectedItems" :value="board.boardId" />
+          </td>
+          <td align="center">
+            {{ board.boardId }}
+          </td>
+          <td align="center">
+            {{ board.boardName }}
+          </td>
+          <td align="center" style="padding: 10px 0;">
+            {{ board.userId.nickname }}
+          </td>
+          <td align="center" style="padding: 10px 0;">
+            {{ board.boardRegisterDate }}
+          </td>
+        </tr>
+      </v-table>
 
       <div class="managementBoardBtn">
         <div class="btn-container">
         <!-- "전체 선택" 버튼에 클릭 이벤트 추가 -->
+        <!-- <div style="width:130px"></div> -->
         <v-btn :color="selectAllButtonColor" @click="toggleSelectAll">{{ selectAllButtonText }}</v-btn>
       </div>
 
@@ -46,15 +47,13 @@
       </div>
 
       <div class="search-container">
-        <input type="text" v-model="searchQuery" placeholder="제목을 입력하세요" />
-        <v-btn @click="search" class="mdi mdi-magnify" variant="text"></v-btn>
+        <input type="text" v-model="searchQuery" placeholder="검색어를 입력하세요" />
       </div>
     </div>
   </div>
 </template>
   
 <script>
-import '@/assets/css/management/managementBoard.css'
 import { mapActions } from 'vuex';
 const BoardModule = 'BoardModule';
 
@@ -79,26 +78,18 @@ export default {
     selectAllButtonColor() {
       return this.selectAll ? "green" : "black";
     },
-    filteredBoards() {
-      if(!this.boards || this.boards.length === 0) {
-        return [];
-      }
-      // 검색어를 소문자로 변환
-      const query = this.searchQuery.toLowerCase();
-      // 검색어와 일치하는 게시물만 필터링
-      return this.boards.filter((board) => {
-        return board.boardName.toLowerCase().includes(query);
-      });
-    },
   },
   watch: {
     selectedItems(newSelectedItems) {
+      // selectedItems 배열이 변경될 때 실행되는 코드
       console.log("선택된 게시물 ID:", newSelectedItems);
     },
   },
   methods: {
     ...mapActions(BoardModule, ['requestManagementBoardDeleteToSpring']),
+    // "전체 선택" 버튼 클릭 시 호출될 메서드
     toggleSelectAll() {
+      // this.selectAll 변수를 토글합니다.
       this.selectAll = !this.selectAll;
       if (this.selectAll) {
         // "전체 선택" 버튼이 눌린 경우, 모든 게시물의 boardId를 selectedItems 배열에 추가하여 선택 상태로 만듭니다.
@@ -108,11 +99,7 @@ export default {
         this.selectedItems = [];
       }
     },
-    search() {
-      console.log("검색어:", this.searchQuery);
-      console.log("검색 결과:", this.filteredBoards);
-    },
-    deleteSelectedItems() {
+   deleteSelectedItems() {
       if (this.selectedItems.length === 0) {
         alert("삭제할 게시물을 선택하세요.");
         return;
@@ -121,10 +108,25 @@ export default {
       console.log("삭제하려고 선택한 : ", boardId)
 
       let boardIds = this.selectedItems.toString()
-      this.requestManagementBoardDeleteToSpring(boardIds);
+
+     this.requestManagementBoardDeleteToSpring(boardIds);
     } 
   },
 };
 </script>
   
-<style lang="scss" scoped></style>
+<style>
+.managementBoardBtn {
+  text-align: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+}
+.search-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+  </style>
+  
