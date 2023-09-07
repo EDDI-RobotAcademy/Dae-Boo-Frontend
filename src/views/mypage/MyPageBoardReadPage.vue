0bb19ca51d8fb27d68e0b2c292146cb4982647f9
cjw0824 <128335544+cjw0824@users.noreply.github.com>
2023-09-07 15:36:59 +0900

    feat:[최지원] 마이페이지 게시물 수정 기능 [PMF-32]

--- src/views/mypage/MyPageBoardReadPage.vue
+++ src/views/mypage/MyPageBoardReadPage.vue
@@ -1,22 +1,35 @@
 <template>
-    <div style="color:black; background-color: white;">
+    <div style="color:black; background-color: white; padding: 30px;">
         <MyPageBoardReadForm v-if="thisBoard" :thisBoard="thisBoard" />
-        <button> 수정 </button>
-        <button @click="deleteBoard"> 삭제 </button>
+        <v-btn class="centerBtn" @click="openModifyModal"> 수정 </v-btn>
+        <v-btn class="centerBtn" @click="deleteBoard"> 삭제 </v-btn>
+
+
+        <div id="modal">
+            <div class="modal-content">
+                <MyPageBoardModify @submit="onSubmit" v-if="thisBoardA" v-model="thisBoardA" :thisBoardA="thisBoardA" />
+                <!-- <MyPageBoardModify @submit="onSubmit" v-if="thisBoardA" v-model="thisBoardA" /> -->
+                <v-btn class="close" color="red" @click="closeModal">x</v-btn>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import { mapActions, mapState } from 'vuex';
 import MyPageBoardReadForm from '../../components/myPage/MyPageBoardReadForm.vue'
+import MyPageBoardModify from '../../components/myPage/MyPageBoardModify.vue'
+import '@/assets/css/myPage/MyPageBoard.css'
 const BoardModule = 'BoardModule'
 export default {
     data() {
         return {
             userId: 1,
+            thisBoardA: null
         }
     },
     components: {
-        MyPageBoardReadForm
+        MyPageBoardReadForm,
+        MyPageBoardModify
     },
     props: {
         boardId: {
@@ -25,20 +38,43 @@ export default {
         },
     },
     methods: {
-        ...mapActions(BoardModule, ['requestBoardReadToSpring', 'requestBoardDeleteToSpring']),
+        ...mapActions(BoardModule, ['requestBoardReadToSpring', 'requestBoardDeleteToSpring', 'requestMyBoardModifyToSpring']),
         deleteBoard() {
             const IdData = {
                 boardId: this.boardId,
                 userId: this.userId
             }
             this.requestBoardDeleteToSpring(IdData);
+        },
+        async onSubmit(payload) {
+            const { boardName, content } = payload
+            const boardId = this.boardId
+            const category = this.thisBoard.category
+            const userId = this.thisBoard.userId
+            // console.log("content: " + content + ", boardName: " + boardName)
+            await this.requestMyBoardModifyToSpring({ category, boardName, content, userId, boardId });
+        },
+        closeModal() {
+            document.getElementById("modal").style.display = "none";
+        },
+        openModifyModal() {
+            document.getElementById("modal").style.display = "block";
         }
     },
     created() {
         this.requestBoardReadToSpring(this.boardId);
     },
     computed: {
-        ...mapState(BoardModule, ['thisBoard'])
+        ...mapState(BoardModule, ['thisBoard']),
+    },
+    watch: {
+        // state의 thisBoard가 변경될 때 thisBoardA에 할당
+        thisBoard: {
+            handler(newThisBoard) {
+                this.thisBoardA = newThisBoard;
+            },
+            immediate: true // 초기에도 실행
+        }
     }
 
 }
