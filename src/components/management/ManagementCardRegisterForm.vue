343d43900b1d864bfbb335106b8de126a52f3f57
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-09-08 11:00:57 +0900

    update: [김재경] 관리자는 카드 등록시 이미지를 등록할 수 있습니다 [PMF-33]

--- src/components/management/ManagementCardRegisterForm.vue
+++ src/components/management/ManagementCardRegisterForm.vue
@@ -72,115 +72,115 @@ const awsBucketRegion = env.api.S3_Region;
 const awsPoolId = env.api.S3_PoolId
 const ManagementModule = 'ManagementModule';
 export default {
-data() {
-return {
-cardRegister: true,
-cardBenefit: '',
-cardCompany: '',
-cardName: '',
-cardCondition: '',
-cardFee: '',
-bucketName: awsBucketName,
-bucketRegion: awsBucketRegion,
-PoolId: awsPoolId,
-s3: null,
-awsFileList: [],
-startAfterAwsS3Bucket: null,
-awsS3NextToken: null,
-localImg: '',
-path: '',
-imgPath: null,
-file: null,
-};
-},
-methods: {
-...mapActions(ManagementModule,['requestCardRegisterToSpring','requestCardListToSpring']),
-async onSubmit() {
-let formData = new FormData()
-let aboutCard = {
-name: this.cardName,
-company: this.cardCompany,
-fee: this.cardFee,
-cardCondition: this.cardCondition,
-benefit: this.cardBenefit,
-cardImage: this.localImg,
-}
-formData.append(
-"aboutCard",
-new Blob([JSON.stringify(aboutCard)], { type: "application/json" })
-)
+  data() {
+    return {
+      cardRegister: true,
+      cardBenefit: '',
+      cardCompany: '',
+      cardName: '',
+      cardCondition: '',
+      cardFee: '',
+      bucketName: awsBucketName,
+      bucketRegion: awsBucketRegion,
+      PoolId: awsPoolId,
+      s3: null,
+      awsFileList: [],
+      startAfterAwsS3Bucket: null,
+      awsS3NextToken: null,
+      localImg: '',
+      path: '',
+      imgPath: null,
+      file: null,
+    };
+  },
+  methods: {
+    ...mapActions(ManagementModule, ['requestCardRegisterToSpring', 'requestCardListToSpring']),
+    async onSubmit() {
+      let formData = new FormData()
+      let aboutCard = {
+        name: this.cardName,
+        company: this.cardCompany,
+        fee: this.cardFee,
+        cardCondition: this.cardCondition,
+        benefit: this.cardBenefit,
+        cardImage: this.localImg,
+      }
+      formData.append(
+        "aboutCard",
+        new Blob([JSON.stringify(aboutCard)], { type: "application/json" })
+      )
 
-try {
-const card = await this.requestCardRegisterToSpring(aboutCard);
-if (card !== null) {
-alert("등록 성공!");
-this.golist();
-this.requestCardListToSpring();
-}
-} catch (error) {
-console.error("등록 중 오류 발생:", error);
-// 오류 처리 추가
-}
-},
-golist() {
-this.$emit('cancel');
-},
-handleFileUpload() {
-this.file = this.$refs.file.files[0]
-console.log('file: ' + this.file.name)
-},
-awsS3Config() {
-AWS.config.update({
-  region: this.bucketRegion,
-  credentials: new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: this.PoolId
-  })
-})
+      try {
+        const card = await this.requestCardRegisterToSpring(aboutCard);
+        if (card !== null) {
+          alert("등록 성공!");
+          this.golist();
+          this.requestCardListToSpring();
+        }
+      } catch (error) {
+        console.error("등록 중 오류 발생:", error);
+        // 오류 처리 추가
+      }
+    },
+    golist() {
+      this.$emit('cancel');
+    },
+    handleFileUpload() {
+      this.file = this.$refs.file.files[0]
+      console.log('file: ' + this.file.name)
+    },
+    awsS3Config() {
+      AWS.config.update({
+        region: this.bucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: this.PoolId
+        })
+      })
 
-this.s3 = new AWS.S3({
-  apiVersion: '2006-03-01',
-  params: {
-    Bucket: this.bucketName
+      this.s3 = new AWS.S3({
+        apiVersion: '2006-03-01',
+        params: {
+          Bucket: this.bucketName
+        }
+      })
+    },
+    uploadAwsS3() {
+      this.awsS3Config();
+      this.s3.upload({
+        Key: this.file.name,
+        Body: this.file,
+        ACL: 'public-read',
+      },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+            return alert('업로드 중 문제 발생 (사진 파일에 문제가 있음)', err.message);
+          }
+          this.localImg = this.file.name;
+          console.log("aws: " + this.localImg);
+          this.$emit('imageUploaded', data.Location);
+          alert('업로드 성공!');
+
+        });
+    },
+    deleteAwsS3File(key) {
+      this.awsS3Config()
+
+      this.s3.deleteObject({
+        Key: key
+      }, (err) => {
+        if (err) {
+          return alert('AWS 버킷 데이터 삭제에 문제가 발생했습니다.: ' + err.message)
+        }
+        alert('AWS 버킷 데이터 삭제가 성공적으로 완료되었습니다.')
+        this.localImg = null
+
+      })
+    },
+
+  },
+  mounted() {
   }
-})
-},
-uploadAwsS3() {
-this.awsS3Config();
-this.s3.upload({
-  Key: this.file.name,
-  Body: this.file,
-  ACL: 'public-read',
-},
-  (err, data) => {
-    if (err) {
-      console.log(err);
-      return alert('업로드 중 문제 발생 (사진 파일에 문제가 있음)', err.message);
-    }
-    this.localImg = this.file.name;
-    console.log("aws: " + this.localImg);
-    this.$emit('imageUploaded', data.Location);
-    alert('업로드 성공!');
-   
-  });
-},
-deleteAwsS3File(key) {
-this.awsS3Config()
-
-this.s3.deleteObject({
-  Key: key
-}, (err) => {
-  if (err) {
-    return alert('AWS 버킷 데이터 삭제에 문제가 발생했습니다.: ' + err.message)
-  }
-  alert('AWS 버킷 데이터 삭제가 성공적으로 완료되었습니다.')
-  this.localImg = null
-
-})
-},
-
-},
-mounted() {
-}
 };
 </script>
 <style lang="">
