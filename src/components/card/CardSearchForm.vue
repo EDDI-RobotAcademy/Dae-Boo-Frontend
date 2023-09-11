a7a1fd2fec9d2d6f9fd1dec113b4391e07839645
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-09-11 16:34:15 +0900

    rename: [김재경] 카드 서치폼 이미지 변수명 변경

--- src/components/card/CardSearchForm.vue
+++ src/components/card/CardSearchForm.vue
@@ -18,7 +18,7 @@
     <div class="container">
       <div class="row">
         <div v-for="card in cards" :key="card.id" class="section">
-          <img :src="card.card_image" alt="card image" class="card-image" data-aos="fade-top" />
+          <img :src="card.cardImage" alt="card image" class="card-image" data-aos="fade-top" />
           <div class="intro">
             <p class="card-name">{{ card.name }}</p>
           </div>
