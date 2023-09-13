7da41344331ce079a68989285d44e2b8daf620c7
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-09-13 12:09:03 +0900

    feat: [장현민] 게시한 게시글을 수정할 수 있습니다 [PMF-44]

---
+++ src/components/board/BoardModifyForm.vue
@@ -0,0 +1,119 @@
+<template>
+    <v-container class="board-modify-container">
+      <form @submit.prevent="onSubmit">
+        <div class="board-read-mr">
+          <v-row align="center" justify="left">
+            <v-col cols="12">
+              <v-text-field
+                label="제목"
+                variant="outlined"
+                class="board-register-body"
+                v-model="boardName"
+              />
+            </v-col>
+            <v-col cols="1">
+              <td class="board-read-text">no. {{ board.boardId }}</td>
+            </v-col>
+            <v-col cols="8">
+              <td class="board-read-text">작성자 :  {{ board.writer }}</td>
+            </v-col>
+            <v-col cols="3">
+              <td class="board-read-text">등록일자 : {{ board.boardRegisterDate }}</td>
+            </v-col>
+            <v-col cols="12">
+              <span class="line" />
+            </v-col>
+            <v-col cols="12">
+              <div id="editor" class="editor"></div>
+            </v-col>
+          </v-row>
+        </div>
+        <div>
+            <v-row>
+            <v-col cols="10">
+          <router-link
+            :to="{
+              name: 'BoardReadPage',
+              params: { boardId: board.boardId },
+            }"
+            style="text-decoration: none; color: white;"
+          >수정 취소</router-link>
+        </v-col>
+        <v-col cols="1">
+          <button @click="onDelete" type="button" style="color: white;">삭제</button>
+        </v-col>
+        <button @click="onSubmit" type="button" style="color: white;">수정 완료</button>
+        <v-col cols="1">
+        </v-col>
+    </v-row>
+        </div>
+      </form>
+    </v-container>
+  </template>
+  
+  <script>
+  import Editor from "@toast-ui/editor";
+  import "@/assets/css/editor-custom-style.css";
+  import "@/assets/css/board/boardModify.css";
+  import "@toast-ui/editor/dist/toastui-editor.css";
+  import { mapActions, mapState } from "vuex";
+  const BoardModule = 'BoardModule';
+  
+  export default {
+    props: {
+      board: {
+        type: Object,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        editor: null,
+        writer: "",
+        boardName: "",
+        content: "",
+      };
+    },
+    computed: {
+      ...mapState(BoardModule, ['board'])
+    },
+    mounted() {
+      this.editor = new Editor({
+        el: document.querySelector("#editor"),
+        height: "500px",
+        initialEditType: "wysiwyg",
+        previewStyle: "vertical",
+        usageStatistics: false,
+        initialValue: this.content,
+        theme: "dark",
+      });
+    },
+    methods: {
+      onSubmit() {
+        this.content = this.editor.getMarkdown();
+        this.$emit("submit", {
+          boardName: this.boardName,
+          writer: this.writer,
+          content: this.content,
+        });
+      },
+      ...mapActions(BoardModule, ['DeleteBoardToSpring']),
+      async onDelete () {
+        const boardId = this.boardId;
+        console.log(boardId)
+        await this.DeleteBoardToSpring(boardId);
+        await this.$router.push({ name: 'BoardListPage' });
+      }
+    },
+    created() {
+      this.boardName = this.board.boardName;
+      this.content = this.board.content;
+      this.writer = this.board.writer;
+    },
+  };
+  </script>
+  
+  <style>
+  
+  </style>
+  
