3c242fbd7f09d7925f6f5badc73443bfb9bfee59
cjw0824 <128335544+cjw0824@users.noreply.github.com>
2023-09-19 20:57:36 +0900

    feat:[최지원] 카카오 지도 api 추가 [PMF-62]

---
+++ src/views/shopSearch/shopSearchPage.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div>
+        <ShopSearchForm/>
+        <!-- <mapTest/> -->
+    </div>
+</template>
+<script>
+import ShopSearchForm from '@/components/shopSearch/ShopSearchForm.vue'
+// import mapTest from '@/components/shopSearch/mapTest.vue'
+export default {
+    components: {
+        ShopSearchForm,
+        // mapTest
+    }
+}
+</script>
+<style lang="">
+    
+</style>
