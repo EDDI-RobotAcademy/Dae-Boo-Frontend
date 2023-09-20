264056ebbf2ffc0e93b8b0d31ac41bae5c82ac09
cjw0824 <128335544+cjw0824@users.noreply.github.com>
2023-09-20 14:42:39 +0900

    fix:[최지원] 카카오 지도 api key 변경 [PMF-62]

--- src/components/shopSearch/mapTest.vue
+++ src/components/shopSearch/mapTest.vue
@@ -21,7 +21,7 @@ export default {
         return {
             placesData: [],
             markers: [],
-            infowindow: new window.kakao.maps.InfoWindow
+            // infowindow: new window.kakao.maps.InfoWindow
         };
     },
     mounted() {
@@ -35,7 +35,7 @@ export default {
         loadScript() {
             const script = document.createElement("script");
             script.src =
-                `//dapi.kakao.com/v2/maps/sdk.js?appkey=7d8a7c6bd0d9cdb29c1a7b876d32b1c2&autoload=false&libraries=services`;
+                `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.VUE_APP_KAKAO_MAP_KEY}&autoload=false&libraries=services`;
             script.onload = () => window.kakao.maps.load(this.loadMap);
 
             document.head.appendChild(script);
@@ -80,12 +80,12 @@ export default {
             this.placesData.forEach((place, index) => {
                 // 마커를 생성하고 지도에 표시합니다
                 const placePosition = new window.kakao.maps.LatLng(place.y, place.x);
-                const marker =
-                    this.addMarker(placePosition, index);
+                // const marker =
+                this.addMarker(placePosition, index);
                 // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
                 // LatLngBounds 객체에 좌표를 추가합니다
                 // bounds.extend(placePosition);
-                this.attachMarkerEvents(marker, place.place_name);
+                // this.attachMarkerEvents(marker, place.place_name);
             });
         },
         addMarker(position, index) {
@@ -104,28 +104,6 @@ export default {
             marker.setMap(this.mapShop);// 지도 위에 마커를 표출
             this.markers.push(marker);// 배열에 생성된 마커를 추가
             return marker;
-        },
-        attachMarkerEvents(marker, content) {
-            // 마커와 검색결과 항목에 mouseover 했을때
-            // 해당 장소에 인포윈도우에 장소명을 표시
-            window.kakao.maps.event.addListener(marker, "mouseover", () => {
-                this.displayInfowindow(marker, content);
-                console.log("content" + content)
-            });
-            // mouseout 했을 때는 인포윈도우를 닫음
-            window.kakao.maps.event.addListener(marker, "mouseout", () => {
-                this.infowindow.close();
-            });
-            // // click 했을 때는 미니윈도우를 토글
-            // window.kakao.maps.event.addListener(marker, "click", () => {
-            //     this.changePopState();
-            //     this.placeData = { title, place_url };
-            // });
-        },
-        displayInfowindow(marker, title) {
-            const content = `<div style="padding:5px;">${title}</div>`;
-            this.infowindow.setContent(content);
-            this.infowindow.open(this.map, marker);
         },
 
     },
