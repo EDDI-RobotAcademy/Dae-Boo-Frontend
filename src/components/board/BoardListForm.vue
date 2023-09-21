ecda2d8de338662cb212255f984e0c6d0b72fb5b
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-09-21 17:04:56 +0900

    update: [장현민] 게시판 리스트에 카테고리 생성 [PMF-41]

--- src/components/board/BoardListForm.vue
+++ src/components/board/BoardListForm.vue
@@ -1,78 +1,94 @@
 <template>
- <v-container class="board-read-style">   
-  <div class="board-mr">
-   <v-btn variant="plain" :ripple="false" class="category-st">전체</v-btn>
-   <v-btn variant="plain" :ripple="false" class="category-st">일상</v-btn>
-   <v-btn variant="plain" :ripple="false" class="category-st">후기</v-btn>
-   <v-table>
-     <thead>
-        <tr align="center" justify="center" class="row-mr">
-          <td width="10%" align="center" class="bottom-line">No.</td>
-          <td width="50%" align="center" class="bottom-line">제목</td>
-          <td width="20%" align="center" class="bottom-line">작성자</td>
-          <td width="20%" align="center" class="bottom-line">작성일</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr class="row-mr" v-if="!boards || (Array.isArray(boards) && boards.length === 0)">
-          <th colspan="4" style="text-align: center;">현재 등록된 게시글이 없습니다!</th>
-        </tr>
-        <tr
-          v-else
-          v-for="(board) in paginatedBoards"
-          :key="board.boardId"
-          @click="boardReadRink(board.boardId)"
-          class="row-mr cardTr"
-        >
-          <td align="center">{{ board.boardId }}</td>
-          <td align="center"> <p style="font-size: small;">[{{ board.category }}]{{ board.boardName }}</p> </td>
-          <td align="center">{{ board.userId.nickname }}</td>
-          <td align="center">{{ board.boardRegisterDate }}</td>
-        </tr>
-      </tbody>
-      <tr>
-        <th colspan="4">
-      <v-pagination
-        v-model="page" :length="Math.ceil(boards.length / itemsPerPage)" class="page-nation"/>
-    </th>
-    </tr>
-    </v-table>
-  </div>
+  <v-container class="board-read-style">   
+   <div class="board-mr">
+    <v-btn variant="plain" :ripple="false" class="category-st" @click="filterByCategory('전체')">전체</v-btn>
+    <v-btn variant="plain" :ripple="false" class="category-st" @click="filterByCategory('DAILY')">DAILY</v-btn>
+    <v-btn variant="plain" :ripple="false" class="category-st" @click="filterByCategory('REVIEW')">REVIEW</v-btn>
+    <v-table>
+      <thead>
+         <tr align="center" justify="center" class="row-mr">
+           <td width="10%" align="center" class="bottom-line">No.</td>
+           <td width="50%" align="center" class="bottom-line">제목</td>
+           <td width="20%" align="center" class="bottom-line">작성자</td>
+           <td width="20%" align="center" class="bottom-line">작성일</td>
+         </tr>
+       </thead>
+       <tbody>
+         <tr class="row-mr" v-if="!filteredBoards || (Array.isArray(filteredBoards) && filteredBoards.length === 0)">
+           <th colspan="4" style="text-align: center;">현재 등록된 게시글이 없습니다!</th>
+         </tr>
+         <tr
+           v-else
+           v-for="(board) in paginatedBoards"
+           :key="board.boardId"
+           @click="boardReadRink(board.boardId)"
+           class="row-mr cardTr"
+         >
+           <td align="center">{{ board.boardId }}</td>
+           <td align="center"> <p style="font-size: small;">[{{ board.category }}]{{ board.boardName }}</p> </td>
+           <td align="center">{{ board.userId.nickname }}</td>
+           <td align="center">{{ board.boardRegisterDate }}</td>
+         </tr>
+       </tbody>
+       <tr>
+         <th colspan="4">
+           <v-pagination
+             v-model="page"
+             :length="Math.ceil(filteredBoards.length / itemsPerPage)"
+             class="page-nation"
+           />
+         </th>
+       </tr>
+     </v-table>
+   </div>
   </v-container>
-</template>
-
-<script>
-import '@/assets/css/board/boardList.css'
-export default {
-  props: {
-    boards: {
-      type: Array,
-      userId: {
-        nickname: String
-      }
-    },
-    
-  },
-  data() {
-    return {
-      page:1,
-      itemsPerPage:10, 
-    }
-  },
-  computed: {
-    paginatedBoards() {
-      const startIndex = (this.page - 1) * this.itemsPerPage;
-      const endIndex = startIndex + this.itemsPerPage;
-      return this.boards.slice(startIndex, endIndex);
-    },
-  },
-  methods: {
-    boardReadRink(boardId) {
-      this.$router.push({path: `/board/read-page/${boardId}`})
-    }
-  },
-};
-</script>
-
-<style lang="scss" scoped>
-</style>
+ </template>
+ 
+ <script>
+ import '@/assets/css/board/boardList.css'
+ export default {
+   props: {
+     boards: {
+       type: Array,
+       userId: {
+         nickname: String
+       }
+     },
+     
+   },
+   data() {
+     return {
+       page: 1,
+       itemsPerPage: 10,
+       selectedCategory: '전체', 
+     }
+   },
+   computed: {
+     filteredBoards() {
+       if (this.selectedCategory === '전체') {
+         return this.boards; 
+       } else {
+         return this.boards.filter(board => board.category === this.selectedCategory);
+       }
+     },
+     paginatedBoards() {
+       const startIndex = (this.page - 1) * this.itemsPerPage;
+       const endIndex = startIndex + this.itemsPerPage;
+       return this.filteredBoards.slice(startIndex, endIndex);
+     },
+   },
+   methods: {
+     boardReadRink(boardId) {
+       this.$router.push({ path: `/board/read-page/${boardId}` })
+     },
+     filterByCategory(category) {
+       this.selectedCategory = category;
+       this.page = 1;
+     },
+   },
+ };
+ </script>
+ 
+ <style lang="scss" scoped>
+ </style>
+ 
