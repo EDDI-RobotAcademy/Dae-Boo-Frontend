ceab6ac64cba28a2e9ff2a2a1dae23c60476b7df
zisoo <zisoo_3@naver.com>
2023-09-23 14:26:54 +0900

    style: [최지수] 상품 상세 페이지 스타일 수정 [PMF-59]

--- src/components/shop/ShopReadProductForm.vue
+++ src/components/shop/ShopReadProductForm.vue
@@ -1,44 +1,80 @@
 <template>
     <div>
-        <div style="display: flex; justify-content: center; align-items: center;">
-            <img class="product" :src="dynamicLink(product.image)" v-if="product.image" />
-        </div>
+        <div class="product-info-container">
+            <div style="display: flex; justify-content: center; align-items: center;">
+                <img class="product-image" :src="dynamicLink(product.image)" v-if="product.image" />
+            </div>
 
-        <div>
-        <V-table>
-            <tr>
-                <td>상품 번호</td>
-                <td>
-                    <input type="text" :value="product.productId" readonly/>
-                </td>
-            </tr>
-            <tr>
-                <td>상품 이름</td>
-                <td>
-                    <input type="text" :value="product.name" readonly/>
-                </td>
-            </tr>
-            <tr>
-                <td>가격</td>
-                <td>
-                    <input type="text" :value="product.price" readonly/>
-                </td>
-            </tr>
-            <tr>
-                <td>상품 설명</td>
-                <td>
-                    <textarea cols="60" rows="20" :value="product.description" readonly/>
-                </td>
-            </tr>
-        </V-table>
-        <router-link :to="{path: '/shopMainpage'}">
-            돌아가기
-        </router-link>
+            <div class="product-table">
+                <V-table>
+                    <tr>
+                        <td class="shopReadProductName">
+                            <div class="shopReadProcutNewBox">NEW</div>
+                            <input type="text" :value="product.name" readonly/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="shopReadProductPrice">
+                            <input type="text" :value="product.price" readonly/>
+                        </td>
+                    </tr>
+                    <td>
+                        <hr style="width:345px; height: 1px; background-color: #ccc; border: none; margin: 5px 0;">
+                    </td>
+                    <br />
+                    <tr>
+                        <td>
+                            <span class="shopReadProductBigText">구매혜택 </span>
+                            <span class="shopReadProductSmallText">포인트 적립예정</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span class="shopReadProductBigText">배송 방법 </span>
+                            <span class="shopReadProductSmallText">택배</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span class="shopReadProductBigText">배송비 </span>
+                            <span class="shopReadProductSmallText">3,000원 | 도서 산간 배송비 추가</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span class="shopReadProductBigText">배송 안내 </span>
+                            <span class="shopReadProductSmallText">기본배송(결제 후 5일 내 배송) / 무료배송(3개이상 구매 시)</span>
+                        </td>
+                    </tr>
+                    <br />
+                    <tr>
+                        <td>
+                            <p class="shopReadProductBigDetail">상품 설명</p>
+                            <textarea class="shopReadProductDetail" cols="50" rows="3" :value="product.description" readonly/>
+                        </td>
+                    </tr>
+                </V-table>
+                <div>
+                    <button class="backBtn">
+                        <router-link :to="{path: '/shopMainpage'}" style="text-decoration: none; color:white">
+                            돌아가기
+                        </router-link>
+                    </button>
+                    <button class="BagBtn" color="orange">
+                        장바구니
+                    </button>
+                </div>
+                <div>
+                    <button class="kakaoPayBtn">카카오페이 구매하기</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import '@/assets/css/shop/shopReadProduct.css'
+
 import AOS from "aos";
 const LINK = process.env.VUE_APP_S3_LINK;
 
