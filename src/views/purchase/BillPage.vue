7b6aecb5b49e7f8e89beea54b35e2f9e7bc4f0af
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-09-25 12:39:52 +0900

    feat: [김재경] 사용자는 주문서에서 주문, 결제를 진행합니다[PMF-63]

--- src/views/purchase/BillPage.vue
+++ src/views/purchase/BillPage.vue
@@ -1,41 +1,64 @@
 <template>
-    <div>
-        <v-container>
-            <v-table style="width:1200px; height: 200px;">
+    
+    <v-container class="table-container">
+        
+        <div class="left-table">
+            <h1>주문 정보</h1>
+            <v-table>
+                <div class="AddressBox">
+                    <AddressComponent :totalAddress="totalAddress" @update:totalAddress="handleTotalAddressUpdate" />
+                </div>
+               
+            </v-table>
+        </div>
+        <div class="right-table">
+            <v-table>
                 <tr>
-                    <th align="center" width="40%">상품정보</th>
-                    <th align="center" width="20%">배송비</th>
+                    <th align="center" width="20%">이미지</th>
+                    <th align="center" width="20%">상품명</th>
+                    <th align="center" width="20%">상품정보</th>
                     <th align="center" width="20%">수량</th>
                     <th align="center" width="20%">가격</th>
                 </tr>
                 <tr>
-                    <td colspan="6">
+                    <td colspan="5">
                         <hr style="margin: 10px 0;"> <!-- 가로 선 밑에 10px 여백 추가 -->
                     </td>
                 </tr>
-                <tr v-if="products">
-                    <td align="center"><span v-html="products.name"></span></td>
-                    <td align="center"><span v-html="products.delivery"></span></td>
-                    <td align="center"><span> <input type="number" v-bind:value="products.amount"></span></td>
-                    <td align="center"><span v-html="products.price"></span></td>
-                    <v-dialog v-model="dialog" persistent max-width="600px">
-                        <template v-slot:activator>
-                            <img :src="this.localImagePath" @click="pay()" alt="로컬 이미지" />
-                        </template>
-                    </v-dialog>
-                </tr>
+                <tr>
+                    <td align="center"><img :src="dynamicLink(product.image)" alt="상품 사진"
+                            style="width: 50px; height: 80px;">
+                    </td>
+                    <td align="center"><span v-html="product.name"></span></td>
+                    <td align="center"><span v-html="product.description"></span></td>
+                    <td align="center"><span> <input type="number" v-bind:value="amount"></span></td>
+                    <td align="center"><span v-html="product.price"></span></td>
 
-                <AddressComponent :totalAddress="totalAddress" @update:totalAddress="handleTotalAddressUpdate" />
+                </tr>
+                <v-dialog class="PayButton" v-model="dialog" persistent>
+                    <template v-slot:activator>
+                        <img :src="this.localImagePath" alt="로컬 이미지" />
+                    </template>
+                </v-dialog>
             </v-table>
-        </v-container>
-    </div>
+        </div>
+
+    </v-container>
 </template>
 <script>
 import AddressComponent from "@/components/address/AddressComponent.vue"
 const PaymentModule = 'PaymentModule'
 const ShopModule = 'ShopModule'
-import { mapActions } from 'vuex';
+const MyPageModule = 'MyPageModule'
+const LINK = process.env.VUE_APP_S3_LINK;
+import { mapActions, mapState } from 'vuex';
 export default {
+    props: {
+        productId: {
+            type: Number,
+            required: true
+        }
+    },
     components: {
         AddressComponent
     },
@@ -45,49 +68,91 @@ export default {
             dialog: false,
             value: '',
             localImagePath: require('@/assets/kakaoPayImg.png'),
-            products: {
-                userId: 1,
-                productId: 1,
-                name: 'aaa',
-                delivery: 3000,
-                amount: 5,
-                price: 5000,
-                userAccount: 'absbas'
-
-            }
+            amount: 1
 
         }
     },
     methods: {
         ...mapActions(PaymentModule, ['requestPaymentToKakao']),
-        ...mapActions(ShopModule, ['requestPurchaseRegisterToSpring']),
+        ...mapActions(ShopModule, ['requestPurchaseRegisterToSpring', 'requestProductToSpring']),
         handleTotalAddressUpdate(newTotalAddress) {
             this.totalAddress = newTotalAddress;
             // 필요한 작업을 수행
         },
-        async pay() {
-            const purchaseForm = {
-                userId: this.products.userId,
-                productId: this.products.productId,
-                userAccount: this.totalAddress,
-                amount: this.products.amount,
-            };
+        dynamicLink(extraPath) {
+            return `${LINK}/${extraPath}`;
+        },
 
-            try {
-                console.log(purchaseForm)
-                let response = await this.requestPurchaseRegisterToSpring(purchaseForm, {
-                    headers: {
-                        'Content-Type': 'application/json' // JSON 데이터를 전송하도록 설정
-                    }
-                });
-                console.log(response.purchaseId);
-                this.requestPaymentToKakao(response)
-            } catch (error) {
-                console.error(error);
-            }
-        }
+        // async pay() {
+        //     const purchaseForm = {
+        //         userId: this.products.userId,
+        //         productId: this.products.productId,
+        //         userAccount: this.totalAddress,
+        //         amount: this.products.amount,
+        //     };
+
+        //     try {
+        //         console.log(purchaseForm)
+        //         let response = await this.requestPurchaseRegisterToSpring(purchaseForm, {
+        //             headers: {
+        //                 'Content-Type': 'application/json' // JSON 데이터를 전송하도록 설정
+        //             }
+        //         });
+        //         console.log(response.purchaseId);
+        //         this.requestPaymentToKakao(response)
+        //     } catch (error) {
+        //         console.error(error);
+        //     }
+        // }
+    },
+    mounted() {
+        // this.userId = this.myInfo.userId;
+
+
+
+    },
+    created() {
+        this.requestProductToSpring(this.productId)
+    },
+    computed: {
+        ...mapState(MyPageModule, ['myInfo']),
+        ...mapState(ShopModule, ["product"]),
+
     }
 
 }
 </script>
+<style>
+.table-container {
+    display: flex;
+    /* 또는 display: grid; 로 설정 가능 */
+    justify-content: space-between;
+    /* 두 테이블 사이에 여백을 주기 위해 사용 */
+    background-color: white;
+    height: 600px;
+}
+
+.left-table,
+.right-table {
+    width: 45%;
+    /* 각 테이블의 너비를 조정하여 오른쪽 여백을 만듭니다. */
+}
+
+.AddressBox {
+    border: 2px solid black;
+    /* 굵은 선, 검은색 테두리 */
+    padding: 20px;
+    /* 내용과 테두리 사이에 여백을 추가할 수 있습니다. */
+    /* 다른 스타일링 속성 추가 가능 */
+}
+
+.PayButton {
+    position: absolute;
+    top: 0;
+    /* 원하는 세로 위치 */
+    right: 0;
+    /* 오른쪽 끝에 배치 */
+    margin-top: 10px;
+    /* 필요한 경우 상단 여백 추가 */
+}</style>
   
