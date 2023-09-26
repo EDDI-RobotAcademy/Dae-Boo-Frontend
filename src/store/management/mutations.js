c496bd380f4e631c3b1d46a6209d774ac1e5ab2f
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-09-26 14:47:49 +0900

    feat: [김재경] 관리자는 판매 내역을 확인할 수 있습니다. [PMF-68]

--- src/store/management/mutations.js
+++ src/store/management/mutations.js
@@ -1,4 +1,4 @@
-import { REQUEST_CARD_LIST_TO_SPRING, REQUEST_ACCOUNT_LIST_TO_SPRING, REQUEST_USER_INFO_TO_SPRING, REQUEST_CARD_INFO_TO_SPRING, REQUEST_Purchase_LIST_TO_SPRING} from "./mutation-types"
+import { REQUEST_CARD_LIST_TO_SPRING, REQUEST_ACCOUNT_LIST_TO_SPRING, REQUEST_USER_INFO_TO_SPRING, REQUEST_CARD_INFO_TO_SPRING, REQUEST_Purchase_LIST_TO_SPRING,REQUEST_REFUND_LIST_TO_SPRING} from "./mutation-types"
 
 export default{
     [REQUEST_CARD_LIST_TO_SPRING](state,receiveData){
@@ -15,6 +15,9 @@ export default{
     },
     [REQUEST_Purchase_LIST_TO_SPRING](state, receiveData){
         state.purchaseList= receiveData;
+    },
+    [REQUEST_REFUND_LIST_TO_SPRING](state, receiveData){
+        state.refundList= receiveData;
     }
 
 }
