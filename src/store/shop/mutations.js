541f6d46750726164884cf7007b8ab89ecc6f57c
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-09-27 17:03:00 +0900

    feat: [김재경] 사용자는 구매 내역을 확인할 수 있습니다[PMF-73]

--- src/store/shop/mutations.js
+++ src/store/shop/mutations.js
@@ -1,6 +1,9 @@
 import {
   REQUEST_PRODUCT_LIST_TO_SPRING,
-  REQUEST_PRODUCT_TO_SPRING
+  REQUEST_PRODUCT_TO_SPRING,
+  REQUEST_PURCHASE_LIST_TO_SPRING,
+  REQUEST_PURCHASE_TO_SPRING,
+  REQUEST_MY_PURCHASE_LIST_TO_SPRING
 } from './mutation-types'
   
 export default {
@@ -10,4 +13,14 @@ export default {
     [REQUEST_PRODUCT_TO_SPRING] (state, receivedData) {
       state.product = receivedData
     },
+    [REQUEST_PURCHASE_LIST_TO_SPRING](state, receivedData){
+      state.purchaseList = receivedData
+    },
+    [REQUEST_PURCHASE_TO_SPRING](state, receivedData){
+      state.purchase = receivedData
+    },
+    [REQUEST_MY_PURCHASE_LIST_TO_SPRING](state, receivedData){
+      state.myPurchaseList = receivedData
+    },
 }
+
