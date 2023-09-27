09d20957e8d25f219b0566e2df1f0fc2ae4bf5f4
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-09-27 14:14:28 +0900

    feat: [김재경] 관리자는 환불 요청에 대해 반려, 환불 을 결정합니다 [PMF-64]

--- src/store/payment/actions.js
+++ src/store/payment/actions.js
@@ -3,16 +3,16 @@ import axiosInst from '@/utility/axiosInst'
 
 export default {
 
-    async requestPaymentToKakao(context, payload){
+    async requestPaymentToKakao(context, payload) {
         await axiosInst.post("/payment/ready", payload)
-        .then(res => {
-            let payUrl = res.data.next_redirect_pc_url
-            console.log(res)
-            location.href = payUrl
-        })
-        .catch(error => {
-            console.error(error);
-    });
+            .then(res => {
+                let payUrl = res.data.next_redirect_pc_url
+                console.log(res)
+                location.href = payUrl
+            })
+            .catch(error => {
+                console.error(error);
+            });
     },
     responsePaymentFailSignal() {
         axiosInst.get('/payment/fail')
@@ -28,7 +28,7 @@ export default {
                     alert('결제가 성공했습니다.');
                 }
             })
-            .catch(() =>{
+            .catch(() => {
                 // 오류 처리
                 console.error;
             });
@@ -47,19 +47,46 @@ export default {
                     alert('결제가 성공했습니다.');
                 }
             })
-            .catch(() =>{
+            .catch(() => {
                 // 오류 처리
                 console.error;
             });
     },
-    async requestPaymentSuccess( _ , payload) {
-     
-        return axiosInst.post("/payment/success",payload) 
+    async requestPaymentSuccess(context, payload) {
+
+        return axiosInst.post("/payment/success", payload)
             .then((res) => {
-                if(res.data==true){
+                if (res.data == true) {
                     router.push({ name: 'BillPage' });
-            
+
                 }
-        })
-    }
+            })
+    },
+    //고객의 환불 요청 액션 페이로드에는 purchaseId가 들어가야 한다
+    async requestPaymentCancelToSpring(context, payload) {
+         await axiosInst.post("/purchase/cancel", payload)
+            .then((res) => {
+                if (res.data == true) {
+                    alert("환불이 신청되었습니다.")
+                }
+            })
+            .catch(() => {
+                console.error
+            })
+
+    },
+    //관리자의 환불 승인
+    async requestPaymentRefundToSpring(context, purchaseId) {
+        const requestData = {purchaseId}
+        await axiosInst.post("/payment/refund", requestData)
+           .then((res) => {
+               if (res.data == true) {
+                   alert("환불이 신청되었습니다.")
+               }
+           })
+           .catch(() => {
+               console.error
+           })
+
+   }
 }
