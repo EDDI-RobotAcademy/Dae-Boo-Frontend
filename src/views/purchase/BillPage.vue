99e44858fe2547b761fba2578cde331e834da35b
zisoo <zisoo_3@naver.com>
2023-09-27 15:43:00 +0900

    style: [최지수] 주문서 스타일을 바꿉니다 [PMF-71]

--- src/views/purchase/BillPage.vue
+++ src/views/purchase/BillPage.vue
@@ -1,56 +1,116 @@
 <template>
-    <v-container class="table-container">
+    <v-container>
+        <div class="billPageContainer">
+            <h2 class="orderSheetTitle">주문서</h2>
+            <div class="orderSheetProductInfo">
+                <p class="orderProductTitle">주문 상품</p>
+                <hr style="color: black; border: 1px solid black;" />
+                <div class="orderSheetProductDetailInfo">
+                    <div class="orderSheetProcutImg">
+                        <img :src="dynamicLink(product.image)" alt="상품 사진">
+                        <p v-html="product.name" style="font-weight: 500; margin: 10px; font-size: small;"></p>
+                    </div>
+                    <span v-html="product.description"></span>
+                    <p> <input type="number" v-bind:value="amount" placeholder="수량을 지정해주세요."></p>
+                    <p v-html="product.price"></p>
+                </div>
+            </div>
 
-        <div class="left-table">
-            <h1>주문 정보</h1>
-            <v-table>
-                <div class="AddressBox">
+            <div class="orderSheetOrdererInfo">
+                <p class="orderUserInfoTitle">주문자 정보</p>
+                <hr style="color: black; border: 1px solid black;" />
+                <div class="orderSheetOrdererDetailInfo">
+                    <div class="orderSheetSenderInfo">
+                        <p class="orderSheetFirstText1">보내는 분</p>
+                        <p>{{myInfo.nickname}}</p>
+                    </div>
+                    <div class="orderSheetSenderPhone">
+                        <p class="orderSheetFirstText2">휴대폰</p>
+                        <p>{{myInfo.mobile}}</p>
+                    </div>
+                    <div class="orderSheetSenderEmail">
+                        <p class="orderSheetFirstText3">이메일</p>
+                        <p v-if="myInfo.email && myInfo.email.includes('@')">{{ myInfo.email }}</p>
+                        <p v-else>이메일 없음</p>
+                    </div>
+                    <div>
+                        <p class="orderSheetFirstText4">
+                            이메일을 통해 주문 처리과정을 보내드립니다.<br>
+                            정보 변경은 내 정보 > 개인정보 수정 메뉴에서 가능합니다.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="orderSheetDeliveryInfo">
+                <p class="orderDeliveryInfoTitle">배송 정보</p>
+                <hr style="color: black; border: 1px solid black;" />
+                <div class="orderSheetDeliveryDetailInfo1">
+                    <p class="orderSheetDeliveryFirstText1">배송지</p>
                     <AddressComponent :totalAddress="totalAddress" @update:totalAddress="handleTotalAddressUpdate" />
                 </div>
+                <div class="orderSheetDeliveryDetailInfo2">
+                    <p class="orderSheetDeliveryFirstText2">배송 요청사항</p>
+                    <p style="font-size: small; font-weight: 600;">안전하게 배달해주세요 !</p>
+                </div>
+            </div>
 
-            </v-table>
+            <div class="orderSheetCouponInfo">
+                <p class="orderCouponInfoTitle">쿠폰</p>
+                <hr style="color: black; border: 1px solid black;" />
+                <div class="orderCouponDetailInfo">
+                    <p class="orderCouponFirstText">쿠폰 적용</p>
+                    <p style="font-size: small; font-weight: 600;">준비중</p>
+                </div>
+            </div>
+
+            <div class="orderSheetPayMentInfo">
+                <p class="orderPayMentInfoTitle">결제 수단</p>
+                <hr style="color: black; border: 1px solid black;" />
+                <div class="orderPayMentDetailInfo">
+                    <p class="orderPayMentFirstText">카카오페이</p>
+                    <div class="orderKakaoPayBox">pay</div>
+                </div>
+            </div>
+
+            <div class="orderSheetPrivacyAgreementInfo">
+                <p class="orderPrivacyInfoTitle">개인정보 수집/제공</p>
+                <hr style="color: black; border: 1px solid black;" />
+                <div class="orderPrivacyFirstText">
+                    <p>
+                        모카 회원 계정으로 카카오페이 이용하여 카카오페이 가맹점에서 제공하는 상품 및 서비스를 구매하고자 할 경우, <br>모카파이낸셜㈜는 거래 당사자간 원활한 의사소통 및 배송, 상담 등 거래이행을 위하여 필요한 최소한의 개인정보를 아래와 같이 제공하고 있습니다.<br>
+                        1. 개인정보를 제공받는 자<br>
+                        상품 및 서비스 판매자<br>
+                        2. 제공하는 개인정보 항목<br>
+                        이름, 모카 아이디, (휴대)전화번호,<br>상품 수령인 정보(배송상품 : 수령인명, 주소, (휴대)전화번호 / E쿠폰 : 이름, 모카 아이디, 휴대전화번호),<br>결제정보(결제수단, 결제금액, 결제일시, 승인유무, 승인번호)<br>
+                        3. 개인정보를 제공받는 자의 이용목적<br>
+                        판매자와 구매자의 원활한 거래 진행, 본인의사의 확인, 고객상담 및 불만처리/부정이용 방지 등의 고객관리, <br>물품배송, 새로운 상품/서비스 정보와 고지사항의 안내, 상품/서비스 구매에 따른 혜택 제공<br>
+                        4. 개인정보를 제공받는 자의 개인정보 보유 및 이용기간<br>
+                        개인정보 이용목적 달성 시까지 보존합니다.<br>단, 관계 법령의 규정에 의하여 일정 기간 보존이 필요한 경우에는 해당 기간만큼 보관 후 삭제합니다.<br>
+                        위 개인정보 제공 동의를 거부할 권리가 있으나, 거부 시 카카오페이를 이용한 상품 및 디지털 콘텐츠 구매가 불가능합니다.<br>그 밖의 내용은 카카오페이 가맹점의 자체 이용약관 및 개인정보처리방침에 따릅니다.<br>
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="right-table">
-            <v-table>
-                <tr>
-                    <th align="center" width="20%">이미지</th>
-                    <th align="center" width="20%">상품명</th>
-                    <th align="center" width="20%">상품정보</th>
-                    <th align="center" width="20%">수량</th>
-                    <th align="center" width="20%">가격</th>
-                </tr>
-                <tr>
-                    <td colspan="5">
-                        <hr style="margin: 10px 0;"> <!-- 가로 선 밑에 10px 여백 추가 -->
-                    </td>
-                </tr>
-                <tr>
-                    <td align="center"><img :src="dynamicLink(product.image)" alt="상품 사진"
-                            style="width: 50px; height: 80px;">
-                    </td>
-                    <td align="center"><span v-html="product.name"></span></td>
-                    <td align="center"><span v-html="product.description"></span></td>
-                    <td align="center"><span> <input type="number" v-bind:value="amount"></span></td>
-                    <td align="center"><span v-html="product.price"></span></td>
 
-                </tr>
-                <v-dialog class="PayButton" v-model="dialog" persistent>
-                    <template v-slot:activator>
-                        <img :src="this.localImagePath" alt="로컬 이미지" />
-                    </template>
-                </v-dialog>
-            </v-table>
-        </div>
-
+        <v-dialog class="PayButton" v-model="dialog" persistent>
+            <template v-slot:activator>
+                <img :src="this.localImagePath" alt="로컬 이미지" />
+            </template>
+        </v-dialog>
     </v-container>
 </template>
+
 <script>
 import AddressComponent from "@/components/address/AddressComponent.vue"
+import { mapActions, mapState } from 'vuex';
+import '@/assets/css/purchase/billPage.css'
+
 const PaymentModule = 'PaymentModule'
 const ShopModule = 'ShopModule'
 const MyPageModule = 'MyPageModule'
 const LINK = process.env.VUE_APP_S3_LINK;
-import { mapActions, mapState } from 'vuex';
+
 export default {
     props: {
         productId: {
@@ -67,8 +127,7 @@ export default {
             dialog: false,
             value: '',
             localImagePath: require('@/assets/kakaoPayImg.png'),
-            amount: 1
-
+            amount:'',
         }
     },
     methods: {
@@ -106,9 +165,6 @@ export default {
     },
     mounted() {
         this.userId = this.myInfo.userId;
-
-
-
     },
     created() {
         this.requestProductToSpring(this.productId)
@@ -116,43 +172,10 @@ export default {
     computed: {
         ...mapState(MyPageModule, ['myInfo']),
         ...mapState(ShopModule, ["product"]),
-
     }
-
 }
 </script>
+
 <style>
-.table-container {
-    display: flex;
-    /* 또는 display: grid; 로 설정 가능 */
-    justify-content: space-between;
-    /* 두 테이블 사이에 여백을 주기 위해 사용 */
-    background-color: white;
-    height: 600px;
-}
-
-.left-table,
-.right-table {
-    width: 45%;
-    /* 각 테이블의 너비를 조정하여 오른쪽 여백을 만듭니다. */
-}
-
-.AddressBox {
-    border: 2px solid black;
-    /* 굵은 선, 검은색 테두리 */
-    padding: 20px;
-    /* 내용과 테두리 사이에 여백을 추가할 수 있습니다. */
-    /* 다른 스타일링 속성 추가 가능 */
-}
-
-.PayButton {
-    position: absolute;
-    top: 0;
-    /* 원하는 세로 위치 */
-    right: 0;
-    /* 오른쪽 끝에 배치 */
-    margin-top: 10px;
-    /* 필요한 경우 상단 여백 추가 */
-}
 </style>
   
