ff0e4d0e054de6a63fe94b5c7c876e0d270d885e
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-10-02 15:24:01 +0900

    feat: [김재경] 카카오페이 결제 중 취소와 실패의 경우 예외 처리 페이지 생성합니다[PMF-76]

---
+++ src/views/purchase/PaymentCancelPage.vue
@@ -0,0 +1,25 @@
+<template lang="">
+ <div class="paymentFail">
+        <h3>결제를 취소하였습니다.</h3>
+        <h3 class="shopText" @click="goShop">쇼핑몰로 돌아가기</h3>
+  
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        goShop(){
+            this.$router.push({name:'ShopMainPage'})
+        }
+    },
+}
+</script>
+<style lang="css">
+.paymentFail{
+    padding-top: 100px;
+    text-align:center;
+}
+.shopText{
+    text-decoration: underline;
+}
+</style>
